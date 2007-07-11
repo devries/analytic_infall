71b18be982627592be7b06d42d97a371fa78a8ba
Christopher De Vries <chris.h.devries@gmail.com>
2007-07-11 15:28:50 -0400

    Partially done with multicomponent hill5.

---
+++ hill5_multicomponent.h
@@ -0,0 +1,15 @@
+/* Solvable 5 parameter hill model, calculates fit and returns rms
+   residual.
+
+   parameters:
+   0: tau_0
+   1: v_lsr
+   2: v_in
+   3: sigma
+   4: Tpeak
+*/
+
+void hill5_multicomponent_init(int channels, double *varray, double *tarray, double nu, double vmin, double vmax, int ncomp, double *comp_voff, double *comp_relint);
+void hill5_multicomponent_free();
+double *hill5_multicomponent_getfit();
+double hill5_multicomponent_evaluate(double *params);
